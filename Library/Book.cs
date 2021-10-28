224c6fbc47caa446b62e77750aff2b40bf08bb26
MercyfulFajd <banfaikaroly@live.com>
2021-10-28 16:53:17 +0200

    Add project files.

---
+++ Library/Book.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Library
+{
+    public class Book
+    {
+       public string Title = "Hajnal";
+    }
+}
---
+++ TestZone/PageTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+
+namespace TestZone
+{
+    [TestClass]
+    public class PageTest
+    {
+        [TestMethod]
+        public void TestPage1()
+        {
+            Page pag = new Page();
+            Assert.AreEqual(1, pag.pageNum, "Testing the Page's number");
+
+        }
+    }
+}
+
+
